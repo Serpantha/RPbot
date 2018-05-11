e054c6fa3a72bbbf79ed6def710bcb1a4dcf2beb
Serpantha <39185776+Serpantha@users.noreply.github.com>
2018-05-11 10:34:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+    console.log('Ready to educate!');
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === '!hello') {
-
-       message.reply('Hello! I am RP bot!');
-
-       }
-
+    	message.reply('Hello! I am RP bot!');
+  	}
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.NDQ0NDExNDY0MzI1NzkxNzU0.DdbiNA.ys5RV2mNy0vHzsVBfFZzpv1bPkU);
